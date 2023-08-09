0e38dae25421be41963ed90db7b502f50ca8ccf2
Karen Rodriguez <scarletrodriguez0701@gmail.com>
2023-08-09 06:28:18 -0500

    Creacion de modelos

---
+++ myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Project(models.Model):
+    '''
+    Modelo que representa un proyecto
+    '''
+    name = models.CharField(max_length=50, help_text='Ingrese el nombre del proyecto')
+
+class Task(models.Model):
+    '''
+    Modelo que representa una tarea de un proyecto
+    '''
+    title = models.CharField( max_length=200, help_text='Ingrese el titulo de la tarea.')
+    description = models.TextField(help_text='Ingrese la descripcion de la tarea.')
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
